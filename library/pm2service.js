e94a1bad39c4fd88b371d0530cca6ff88411b30d
Javier Palacios <javiplx@gmail.com>
2017-11-25 15:47:49 +0100

    Minimal argument parsing

--- library/pm2service.js
+++ library/pm2service.js
@@ -4,3 +4,28 @@
 
 var pm2 = require('pm2');
 
+
+// Parse input arguments
+
+var arguments = new Hash(process.argv.slice(2));
+
+var ansible_opts = Object.keys(arguments).filter(function(key){
+  arguments[key].startsWith("_");
+  });
+
+var module_args = Object.keys(arguments).filter(function(key){
+  ! arguments[key].startsWith("_");
+  });
+
+
+var result = { "changed": false }
+
+
+// Verify required arguments
+
+if ( ! module_args.name ) {
+  result.error = "Missing required argument 'name'";
+  console.log(JSON.stringify(result));
+  process.exit()
+  }
+
