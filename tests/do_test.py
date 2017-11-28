ef46c49e584dceca5fb887077d23bc83e790df79
Javier Palacios <javiplx@gmail.com>
2017-11-28 20:21:19 +0100

    Add function to retrieve the list of pm2 processes

--- tests/do_test.py
+++ tests/do_test.py
@@ -16,6 +16,14 @@ library = "library/pm2service.js"
 testfile = "tests.yml"
 
 
+def pm2list ( name=None ) :
+    processes = json.loads( subprocess.check_output(["/usr/bin/pm2", "jlist"]) )
+    if name :
+        [ p for p in processes if p["name"] == name ][0]
+    else :
+        processes
+
+
 doc = filter( lambda t : not t.get("skip", False) , yaml.load(open(testfile)) )
 
 outputs = {}
