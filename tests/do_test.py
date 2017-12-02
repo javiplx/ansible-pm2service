95d735e7aa7c7c5f41f6dbc1baecdd98ce4732c4
Javier Palacios <javiplx@gmail.com>
2017-12-02 14:26:35 +0100

    Execute tests once a time, adding setUp & tearDown options

--- tests/do_test.py
+++ tests/do_test.py
@@ -13,7 +13,7 @@ import subprocess
 import os
 
 library = "library/pm2service.js"
-testfile = "tests.yml"
+testfile = "tests/tests.yml"
 
 
 def pm2service ( name ) :
@@ -41,10 +41,6 @@ def pm2servicechange ( change , before , after ) :
 
 doc = filter( lambda t : not t.get("skip", False) , yaml.load(open(testfile)) )
 
-outputs = {}
-fd = tempfile.NamedTemporaryFile()
-fd.write( "cd %s\n" % os.path.realpath('..') )
-
 for test in doc :
     testfile = tempfile.NamedTemporaryFile()
     args_data = ""
@@ -52,28 +48,24 @@ for test in doc :
         args_data += '%s=%s ' % (k, shlex_quote(text_type(v)))
     data = to_bytes(args_data, errors='surrogate_or_strict')
     testfile.write(data)
-    outputs[test["name"]] = testfile
-    fd.write( "node {0} {1} > {1}.stdout 2> {1}.stderr\n".format(library, testfile.name) )
     testfile.flush()
+    if test.has_key( "setUp" ) :
+        os.system( "%s > /dev/null 2>&1" % test["setUp"] )
     if test.has_key("pm2service") :
         test['before'] = pm2service( test["pm2service"][1:] )
+    os.system( "node {0} {1} > {1}.stdout 2> {1}.stderr\n".format(library, testfile.name) )
 
-fd.flush()
-os.system( "bash %s" % fd.name )
-
-for test in doc :
-    testfile = outputs[test["name"]].name
-    if os.stat("%s.stderr"%testfile).st_size != 0 :
+    if os.stat("%s.stderr"%testfile.name).st_size != 0 :
         print( "%s : ERROR, stderr not empty" % test["name"])
     else :
-        os.unlink("%s.stderr"%testfile)
-        output = json.load(open("%s.stdout"%testfile))
+        os.unlink("%s.stderr"%testfile.name)
+        output = json.load(open("%s.stdout"%testfile.name))
         if test.has_key("expect_fail") :
             failmsg = test["expect_fail"] or ""
             if output.get("failed") :
                 if failmsg.lower() in output.get("msg", "").lower() :
                     print( "%s : OK" % test["name"] )
-                    os.unlink("%s.stdout"%testfile)
+                    os.unlink("%s.stdout"%testfile.name)
                 else :
                     print( "%s : ERROR, '%s' not present in failure message '%s'" % ( test["name"] , failmsg , output.get("msg", "NO ERROR MESSAGE GIVEN") ) )
             else :
@@ -87,12 +79,15 @@ for test in doc :
                         after = pm2service( test["pm2service"][1:] )
                         if pm2servicechange( test["pm2service"][0] , test["before"] , after ) :
                             print( "%s : OK" %  test["name"] )
-                            os.unlink("%s.stdout"%testfile)
+                            os.unlink("%s.stdout"%testfile.name)
                         else :
                             print( "%s : ERROR, %s" % ( test["name"] , output.get("msg", "Unexpected change in service state")) )
                     except Exception, ex:
                         print( "%s : EXCEPTION, %s" % ( test["name"] , ex ) )
                 else :
                     print( "%s : OK" %  test["name"] )
-                    os.unlink("%s.stdout"%testfile)
+                    os.unlink("%s.stdout"%testfile.name)
+
+    if test.has_key( "tearDown" ) :
+        os.system( "%s > /dev/null 2>&1" % test["tearDown"] )
 
